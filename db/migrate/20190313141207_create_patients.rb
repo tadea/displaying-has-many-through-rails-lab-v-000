6643f93c3e6fc868a8ba52333c7c4e65d281a85e
Tadea Simunovic <tsimunoviczg@gmail.com>
2019-03-13 18:21:12 +0000

    Automatically backed up by Learn

--- db/migrate/20190313141207_create_patients.rb
+++ db/migrate/20190313141207_create_patients.rb
@@ -2,7 +2,7 @@ class CreatePatients < ActiveRecord::Migration
   def change
     create_table :patients do |t|
       t.string :name
-      t.integer :age
+      t.string :age
       t.timestamps null: false
     end
   end
